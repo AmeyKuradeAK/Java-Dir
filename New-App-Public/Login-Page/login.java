c86ac62d7ac6704fe3120bd4e38b381ba489b945
AmeyKuradeAK <ameykurade60@gmail.com>
2023-08-27 12:47:40 +0530

    B changed

---
+++ New-App-Public/Login-Page/login.java
@@ -0,0 +1,12 @@
+import stdnames.StdNames;
+
+public class login {
+    public static void main(String[] args) {
+        StdNames obj = new StdNames();
+        obj.login();
+        
+
+        
+    
+    }
+}
